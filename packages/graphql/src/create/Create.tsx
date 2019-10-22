20ee77b6a500b8e9048bf343e546e8968dbd609c
Neilime <escalle.emilien@gmail.com>
2019-10-22 16:05:47 +0200

    Add graphql package

---
+++ packages/graphql/src/create/Create.tsx
@@ -0,0 +1,23 @@
+import { MutationFetchResult, useMutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+type IUseCreateCallback<Result> = (mutation: string) => Promise<Result>;
+
+export const useCreateCallback: IUseCreateCallback<any> = async (mutation: string) => {
+
+    const [mutationCallback] = useMutation(gql`${mutation}`);
+
+    return async (values: any) => {
+        const response: void | MutationFetchResult = await mutationCallback({
+            variables: {
+                data: values,
+            },
+        });
+
+        if (!response || !response.data) {
+            throw new Error('An error has occured');
+        }
+
+        return response.data[Object.keys(response.data)[0]];
+    };
+};
