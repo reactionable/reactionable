bcee39be27af6271a9be21123cee182b2ab572d6
Neilime <escalle.emilien@gmail.com>
2019-11-18 13:41:29 +0100

    fix(amplify): fix useQueryList hook
    Refetch should reset current list

--- packages/core/src/crud/list/QueryList.tsx
+++ packages/core/src/crud/list/QueryList.tsx
@@ -3,6 +3,7 @@ import { IUseQueryResult, IUseQueryOptions } from '../Query';
 export interface IUseQueryListResult<Data> extends Omit<IUseQueryResult<Data>, 'data'> {
     data: Array<Data>;
     next: () => void;
+    previous: () => void;
 };
 
 export type IUseQueryList<Data extends {}, Options extends IUseQueryOptions = {}> = (
