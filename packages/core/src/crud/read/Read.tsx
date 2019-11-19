473575c8b5e7c0d7f0cf0104808ea46612ed2105
Neilime <escalle.emilien@gmail.com>
2019-11-19 19:54:41 +0100

    fix: read children data must not be undefined
    fix #3

--- packages/core/src/crud/read/Read.tsx
+++ packages/core/src/crud/read/Read.tsx
@@ -1,14 +1,14 @@
 import React, { ReactElement } from 'react';
-import { IQueryWrapperProps, QueryWrapper, IUseQueryResult } from '../query/Query';
+import { IQueryWrapperProps, QueryWrapper, IUseQueryResult, IQueryWrapperChildrenProps } from '../query/Query';
 
-export type IReadProps<Data> = Omit<IQueryWrapperProps<Data>, 'children'> & {
-    children: (data?: Data) => ReactElement;
+export type IReadProps<Data> = Omit<IQueryWrapperProps<IUseQueryResult<Data>>, 'children'> & {
+    children: (data: Data) => ReactElement;
 };
 
 export type ReadComponent<Data> = React.FC<IReadProps<Data>>;
 
 export function Read<Data>({ children, ...props }: IReadProps<Data>) {
-    const renderChildren = (props: IUseQueryResult<Data>) => {
+    const renderChildren = (props: IQueryWrapperChildrenProps<IUseQueryResult<Data>>) => {
         return children(props.data);
     };
     return <QueryWrapper<Data> {...props} children={renderChildren} />;
